65c6a7700ab0f3719f5e4f7429c9d0f26bb840ec
Preludium <44121335+Preludium@users.noreply.github.com>
2018-11-18 13:10:11 +0100

    Add files via upload

--- client.c
+++ client.c
@@ -10,40 +10,62 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define MAX 100
+#define MAX 1025
 
 int main()
 {
-    char server_name[] = "ser_fifo", client_name[] = "Kupa", command[MAX];
+    char server_name[] = "ser_fifo", client_name[MAX], command[MAX];
     int fds, fdc;
+    
     while(1)    
     {
-        //printf("Client fifo name : ");
-        //scanf("%s", client_name);
+        printf("Client fifo name : ");
+        fgets(client_name, sizeof(client_name), stdin);
+
+        if ((strlen(client_name) > 0) && (client_name[strlen (client_name) - 1] == '\n'))
+            client_name[strlen (client_name) - 1] = '\0';
 
         unlink(client_name);
+
         if(mkfifo(client_name, S_IRUSR | S_IWUSR) == -1)
         {
             perror("client fifo create error");
             exit(1);
         }  
-
+        
         printf("Command : ");
-        fgets(command, MAX, stdin);
+        fgets(command, sizeof(command), stdin);
+
+        // if ((strlen(command) > 0) && (command[strlen (command) - 1] == '\n'))
+        //     command[strlen (command) - 1] = '\0';
+
+        // printf("%s\n", command);
+
 
         if((fdc = open(client_name, O_RDWR)) == -1)
         {
             perror("client fifo open error");
             exit(1);
         }  
-        write(fdc, command, MAX);
+        write(fdc, command, MAX);        
+
+        // free(command);
 
         if((fds = open(server_name, O_RDWR)) == -1)
         {
             perror("server fifo open error");
             exit(1);
         } 
-        write(fds, client_name, MAX);   
+        write(fds, client_name, MAX); 
+
+        // return 0;
+
+        // if((fdc = open(server_name, O_RDWR)) == -1)
+        // {
+        //     perror("server fifo open error");
+        //     exit(1);
+        // } 
+        // read(fdc, stdout, MAX);
         return 0; 
     }
     return 0;
--- server.c
+++ server.c
@@ -10,13 +10,13 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define MAX 100
+#define MAX 1025
 
 int main()
 {
     char server_name[] = "ser_fifo", client_name[MAX], command[MAX];
     int fds, fdc;
-    int i, j;
+    int i, j, status;
     char * args[MAX];
     unlink(server_name);
 
@@ -28,21 +28,25 @@ int main()
     
     while(1)
     {
+        printf("otwiera server\n");
         if((fds = open(server_name, O_RDWR)) == -1)
         {
             perror("server fifo open error");
             exit(1);
         } 
+        printf("czyta z servera\n");
         read(fds, client_name, sizeof(client_name));
-        //unlink(client_name);
-
+        // close(fds);
+        // unlink(client_name);
+        printf("otwiera clienta %s\n", client_name);
         if((fdc = open(client_name, O_RDWR)) == -1)
         {
             perror("client fifo open error");
             exit(1);
         }
+        printf("czyta z clienta\n");
         read(fdc, command, sizeof(command));
-
+        printf("nie zesralo sie tym razem\n");
         i = 0;
         args[0] = &command[0];
         j = 1;
@@ -58,13 +62,22 @@ int main()
         }
         command[i] = '\0';
         args[j] = NULL;  
-
-        dup2(fds, 0);
+        printf("dup2\n");
+        dup2(fds, fdc);
         if(fork() == 0)
         {
             execvp(args[0], args);
-            exit(0);
-        }      
+            exit(-1);
+        }     
+        wait(&status);
+        if(WEXITSTATUS(status) != 0)
+        {
+            printf("Command \" ");
+            for(int l=0; l<j;l++)
+                printf("%s ",args[l]);
+            printf("\" not found\n");
+        } 
+        // memset(client_name, 0, sizeof(client_name));    
     }
     return 0;   
 }
