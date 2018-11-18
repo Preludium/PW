1e9850da2690defe73e69d58950500ebfcaee45d
Preludium <44121335+Preludium@users.noreply.github.com>
2018-11-18 14:35:33 +0100

    cos zaczelo smigac

--- client.c
+++ client.c
@@ -12,26 +12,33 @@
 
 #define MAX 1025
 
-int main()
+int main(int argc, char* argv[])
 {
-    char server_name[] = "ser_fifo", client_name[MAX], command[MAX];
-    int fds, fdc;
-    
+    char server_name[] = "ser_fifo", client_name[sizeof(argv[1])], command[MAX];
+    int fds, fdc, n;
+    char buf[MAX];
+
+    strcpy(client_name, argv[1]);
+
+    unlink(client_name);
+
+    if(mkfifo(client_name, S_IRUSR | S_IWUSR) == -1)
+    {
+        perror("client fifo create error");
+        exit(1);
+    }
+
     while(1)    
     {
-        printf("Client fifo name : ");
-        fgets(client_name, sizeof(client_name), stdin);
+        // printf("Client fifo name : ");
+        // fgets(client_name, sizeof(client_name), stdin);
 
-        if ((strlen(client_name) > 0) && (client_name[strlen (client_name) - 1] == '\n'))
-            client_name[strlen (client_name) - 1] = '\0';
+        // if ((strlen(client_name) > 0) && (client_name[strlen (client_name) - 1] == '\n'))
+        //     client_name[strlen (client_name) - 1] = '\0';
 
-        unlink(client_name);
+        // unlink(client_name);
 
-        if(mkfifo(client_name, S_IRUSR | S_IWUSR) == -1)
-        {
-            perror("client fifo create error");
-            exit(1);
-        }  
+          
         
         printf("Command : ");
         fgets(command, sizeof(command), stdin);
@@ -41,23 +48,40 @@ int main()
 
         // printf("%s\n", command);
 
+        // printf("otwieranie clienta\n");
+        // if((fdc = open(client_name, O_RDWR)) == -1)
+        // {
+        //     perror("client fifo open error");
+        //     exit(1);
+        // }  
+        // printf("pisanie do clienta\n");
+        // write(fdc, command, strlen(command));        
+        // close(fdc);
 
-        if((fdc = open(client_name, O_RDWR)) == -1)
-        {
-            perror("client fifo open error");
-            exit(1);
-        }  
-        write(fdc, command, MAX);        
-
-        // free(command);
-
-        if((fds = open(server_name, O_RDWR)) == -1)
+        // printf("otwieranie servera\n");
+        if((fds = open(server_name, O_WRONLY)) == -1)
         {
             perror("server fifo open error");
             exit(1);
         } 
-        write(fds, client_name, MAX); 
+        // printf("pisanie do servera\n");
+        write(fds, command, strlen(command)); 
+        close(fds);
 
+
+        // printf("otwieranie clienta\n");
+        if((fdc = open(client_name, O_RDONLY)) == -1)
+        {
+            perror("client fifo open error");
+            exit(1);
+        }  
+        // printf("czytanie z clienta\n");
+        while((n = read(fdc, buf, sizeof(buf))) > 0) 
+        {
+            write(1, buf, n);
+            // printf("W petli read\n");
+        }    
+        close(fdc);   
         // return 0;
 
         // if((fdc = open(server_name, O_RDWR)) == -1)
@@ -66,7 +90,8 @@ int main()
         //     exit(1);
         // } 
         // read(fdc, stdout, MAX);
-        return 0; 
+        // return 0; 
+        // printf("koniec petli\n");
     }
     return 0;
 }
--- server.c
+++ server.c
@@ -14,9 +14,9 @@
 
 int main()
 {
-    char server_name[] = "ser_fifo", client_name[MAX], command[MAX];
+    char server_name[] = "ser_fifo", client_name[] = "Kupa", command[MAX];
     int fds, fdc;
-    int i, j, status;
+    int i, j;//, status;
     char * args[MAX];
     unlink(server_name);
 
@@ -29,24 +29,26 @@ int main()
     while(1)
     {
         printf("otwiera server\n");
-        if((fds = open(server_name, O_RDWR)) == -1)
+        if((fds = open(server_name, O_RDONLY)) == -1)
         {
             perror("server fifo open error");
             exit(1);
         } 
         printf("czyta z servera\n");
-        read(fds, client_name, sizeof(client_name));
-        // close(fds);
-        // unlink(client_name);
-        printf("otwiera clienta %s\n", client_name);
-        if((fdc = open(client_name, O_RDWR)) == -1)
-        {
-            perror("client fifo open error");
-            exit(1);
-        }
-        printf("czyta z clienta\n");
-        read(fdc, command, sizeof(command));
-        printf("nie zesralo sie tym razem\n");
+        read(fds, command, sizeof(command));
+        close(fds);
+
+        // printf("otwiera clienta %s\n", client_name);
+        // if((fdc = open(client_name, O_RDWR)) == -1)
+        // {
+        //     perror("client fifo open error");
+        //     exit(1);
+        // }
+        // printf("czyta z clienta %s\n", client_name);
+        // read(fdc, command, sizeof(command));
+        // printf("nie zesralo sie tym razem\n");
+        // close(fdc);
+
         i = 0;
         args[0] = &command[0];
         j = 1;
@@ -62,21 +64,41 @@ int main()
         }
         command[i] = '\0';
         args[j] = NULL;  
-        printf("dup2\n");
-        dup2(fds, fdc);
+
+        if((fdc = open(client_name, O_WRONLY)) == -1)
+        {
+            perror("client fifo open error");
+            exit(1);
+        }
+
+        // printf("dup2\n");
+        dup2(fdc, 1);
         if(fork() == 0)
         {
-            execvp(args[0], args);
-            exit(-1);
+            if(execvp(args[0], args) == -1)
+            {
+                printf("Command \" ");
+                for(int l=0; l<j;l++)
+                    printf("%s ",args[l]);
+                printf("\" not found\n");
+                exit(1);
+            }
+            exit(0);
         }     
-        wait(&status);
-        if(WEXITSTATUS(status) != 0)
+        // wait(&status);
+        // if(WEXITSTATUS(status) != 0)
+        // {
+            
+        // } 
+        if (wait(NULL) == -1) 
         {
-            printf("Command \" ");
-            for(int l=0; l<j;l++)
-                printf("%s ",args[l]);
-            printf("\" not found\n");
-        } 
+            perror("wait on child");
+            exit(1);
+        }
+        close(fdc);
+        close(1);
+        printf("koniec petli servera\n");
+
         // memset(client_name, 0, sizeof(client_name));    
     }
     return 0;   
