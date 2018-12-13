6bb6bdf119e70e439584034e4b99803738776551
Mikolaj Szymczak <szymczak.mikolaj08@gmail.com>
2018-12-13 13:18:52 +0100

    Update msg.c

--- msg.c
+++ msg.c
@@ -18,15 +18,15 @@
 struct msgbuf 
 {
     long mtype;
-    char mtext[1024];
+    int mdane;
 };
 
 int main()
 {
     int id;
-    if((id = msgget(17, IPC_CREAT | 0600)) == -1)
+    if((id = msgget(0x250, IPC_CREAT | 0600)) == -1)
     {
-        perror("tworzenie kolejki komunikatow");
+        perror("Create message queue error");
         exit(1);
     }
 
@@ -34,48 +34,58 @@ int main()
     {
         struct msgbuf x;
         x.mtype = 10;
-        strcpy(x.mtext, "Hello1\n");
-
-        // while(1)
-        // {
-            if(msgsnd(id, &x, 1025,0) == -1)
+        while(1)
+        {
+            printf("Enter the number : ");
+            scanf("%d", &x.mdane);
+            
+            if(msgsnd(id, &x, sizeof(int), 0) == -1)
             {
-                perror("wysylanie wiadmosci 1");
+                perror("Client send message error");
                 exit(1);
             }
-
-            // if(msgrcv(id, &x, 7, 0, IPC_NOWAIT) == -1)
-            // {
-            //     perror("odbieranie wiadomosci 1");
-            //     exit(1);
-            // }
-        // }
+            
+            if(x.mdane == 0)
+            {
+                if(msgrcv(id, &x, sizeof(int), 10, 0) == -1)
+                {
+                    perror("Client receive message error");
+                    exit(1);
+                }
+                printf("The sum is %d\n", x.mdane);
+                exit(0);
+            }
+        }
     }
 
     if(fork() == 0)
     {
         struct msgbuf y;
         y.mtype = 10;
-        // strcpy(y.mtext, "Hello2\n");
-        // char *msg[1025];
-        // while(1)
-        // {
-            if(msgrcv(id, &y, 1025, 10, 0) == -1)
+        int sum = 0;
+        while(1)
+        {
+            if(msgrcv(id, &y, sizeof(int), 10, 0) == -1)
             {
-                perror("odbieranie wiadomosci 2");
+                perror("Server receive message error");
                 exit(1);
             }
 
-            // write(1, y.mtext, 1025);
-            printf("%s\n", y.mtext);
-
-            // if(msgsnd(id, &y, 7, IPC_NOWAIT) == -1)
-            // {
-            //     perror("wysylanie wiadmosci 2");
-            //     exit(1);
-            // }
-        // }
+            if(y.mdane != 0)
+                sum += y.mdane;
+            else
+            {
+                y.mdane = sum;
+                if(msgsnd(id, &y, sizeof(int), 0) == -1)
+                {
+                    perror("Server send message error");
+                    exit(1);
+                }
+                exit(0);
+            }
+        }
     }
-    // while(1);
+    wait(NULL);
+    wait(NULL);
     return 0;
 }
