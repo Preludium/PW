836cbe8c4f6719502cd3e0bf0c71ca4d448ba997
Mikolaj Szymczak <szymczak.mikolaj08@gmail.com>
2018-12-13 20:33:18 +0100

    Update msg.c

--- msg.c
+++ msg.c
@@ -15,6 +15,10 @@
 #include <sys/sem.h>
 #include <sys/msg.h>
 
+#define M_Dane 1
+#define M_End 2
+#define M_Wynik 3
+
 struct msgbuf 
 {
     long mtype;
@@ -24,7 +28,7 @@ struct msgbuf
 int main()
 {
     int id;
-    if((id = msgget(0x250, IPC_CREAT | 0600)) == -1)
+    if((id = msgget(0x123, IPC_CREAT | 0600)) == -1)
     {
         perror("Create message queue error");
         exit(1);
@@ -33,25 +37,32 @@ int main()
     if(fork() == 0)
     {
         struct msgbuf x;
-        x.mtype = 10;
         while(1)
         {
             printf("Enter the number : ");
             scanf("%d", &x.mdane);
-            
+
+            if(x.mdane != 0)
+                x.mtype = M_Dane;
+            else
+                x.mtype = M_End;
+
             if(msgsnd(id, &x, sizeof(int), 0) == -1)
             {
                 perror("Client send message error");
                 exit(1);
             }
             
-            if(x.mdane == 0)
+            if(msgrcv(id, &x, sizeof(int), 0, 0) == -1)
             {
-                if(msgrcv(id, &x, sizeof(int), 10, 0) == -1)
-                {
-                    perror("Client receive message error");
-                    exit(1);
-                }
+                perror("Client receive message error");
+                exit(1);
+            }
+ 
+            if(x.mtype == M_Dane)
+                printf("The sum is %d\n", x.mdane);
+            else if(x.mtype == M_Wynik)
+            {
                 printf("The sum is %d\n", x.mdane);
                 exit(0);
             }
@@ -61,21 +72,29 @@ int main()
     if(fork() == 0)
     {
         struct msgbuf y;
-        y.mtype = 10;
         int sum = 0;
         while(1)
         {
-            if(msgrcv(id, &y, sizeof(int), 10, 0) == -1)
+            if(msgrcv(id, &y, sizeof(int), 0, 0) == -1)
             {
                 perror("Server receive message error");
                 exit(1);
             }
 
-            if(y.mdane != 0)
+            if(y.mtype == M_Dane)
+            {
                 sum += y.mdane;
-            else
+                y.mdane = sum;
+                if(msgsnd(id, &y, sizeof(int), 0) == -1)
+                {
+                    perror("Server send message error");
+                    exit(1);
+                }
+            }
+            else if(y.mtype == M_End)
             {
                 y.mdane = sum;
+                y.mtype = M_Wynik;
                 if(msgsnd(id, &y, sizeof(int), 0) == -1)
                 {
                     perror("Server send message error");
