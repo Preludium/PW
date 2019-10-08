f0436d0e97eaab935e22581e979db726b9597f5b
Mikołaj Szymczak <44121335+Preludium@users.noreply.github.com>
2019-10-08 11:15:40 +0200

    Add files via upload

--- zad1.cpp
+++ zad1.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(int argc, char **argv) {
     int file;
 
-    uint16_t hostEndianessPort = 13;
+    uint16_t hostEndianessPort = 19;
 
     if((file = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1)
     {
@@ -21,14 +21,21 @@ int main(int argc, char **argv) {
     }
     sockaddr_in addr;
     addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_addr.s_addr = inet_addr("150.254.32.149");
     addr.sin_port = htons(hostEndianessPort);
     
-    connect(file, (sockaddr*) &addr, sizeof(addr));
+    if(connect(file, (sockaddr*) &addr, sizeof(addr)) == -1){
+        perror("Connect error\n");
+        return 0;
+    }
 
     char buff[64];
-    while(int n = read(file, buff, sizeof(buff)))
+    while(int n = recv(file, buff, sizeof(buff), MSG_WAITALL))
     {
+        if (n == -1){
+            perror("Read error\n");
+            return 0;
+        }
         write(1, buff, n);
     }
 
